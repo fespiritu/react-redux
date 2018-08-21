f3a8fa88c0e12194bdc0237400ff49afff34ca49
Ferdinand Espiritu <freddie.espiritu@yahoo.com>
2018-08-20 20:34:16 -0700

    Added TopAppBar

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 //redux stuffs
-import {Provider} from 'react-redux'
-import configureStore from './store'
-
+import { Provider } from "react-redux";
+import configureStore from "./store";
 
 ReactDOM.render(
-    <Provider store={configureStore()}><App />
-    </Provider>, document.getElementById('root')
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 registerServiceWorker();
